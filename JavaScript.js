bd87b8b410fd9ca633dbdea9f7c60167a1a58325
Delcii <119106034+Delcii@users.noreply.github.com>
2023-05-28 12:34:31 +0000

    palla che addirittura si muove

--- JavaScript.js
+++ JavaScript.js
@@ -13,36 +13,42 @@ persona.src = "stickman.png";
 const palla = new Image();
 palla.src = "palla.png";
 
+const risultato = document.getElementById("risultato");
+const pulsante = document.getElementById("pulsante");
+
 function drawProjectileMotion() {
+    pulsante.disabled = true;
+    risultato.innerHTML = "";
+    let canestro = new Boolean(false);
     let x = 0;
     let y = 0;
     let t = 0;
 
     const interval = setInterval(() => {
-
         context.clearRect(0, 0, canvas.width, canvas.height);
-
         context.beginPath();
+        context.strokeStyle = "black";
         context.moveTo(500, 300);
         context.lineTo(580, 300);
         context.stroke();
 
         context.drawImage(persona, -110, 330, 350, 350 * persona.height / persona.width);
-
         t += fps;
         x = velocita.value * Math.cos((angolo.value * Math.PI) / 180) * t;
         y = velocita.value * Math.sin((angolo.value * Math.PI) / 180) * t - (0.5 * 9.81 * t * t);
-
-        context.beginPath();
-        let pat = context.createPattern(palla, "no-repeat");
-        context.strokeStyle = pat;
-        context.arc((x * scale)+10, (canvas.height - y * scale)-50, 5, 0, 2 * Math.PI);
-        context.fill();
-
-        if (y <= 0) {
+        posX = (x*scale)+10;
+        posY = (canvas.height - y * scale)-70;
+        context.drawImage(palla, posX, posY, 35, 35);
+        if(posX>=500 && posX<=580 && Math.abs(posY-300) < 5)    canestro = true;
+        if ((canvas.height - y * scale) >= 535 || (x*scale)+10 >= 700) {
             clearInterval(interval);
+            if(canestro == true)    risultato.innerHTML = "Bel canestro!";
+            else    risultato.innerHTML = "Mancato!";
+            pulsante.disabled = false;
         }
     }, fps * 1000);
+    
+    
 }
 
 function inizio() {
@@ -51,5 +57,5 @@ function inizio() {
     context.lineTo(580, 300);
     context.stroke();
     context.drawImage(persona, -110, 330, 350, 350 * persona.height / persona.width);
-    context.drawImage(palla, 200, 200, 50, 50 * palla.height / palla.width);
+    context.drawImage(palla, -15, 420, 35, 35);
 }
